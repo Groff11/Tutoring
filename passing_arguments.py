998e9bba3a01ad1c2b57c4eb99bddf942bd36316
major <echolabstech@gmail.com>
2019-04-26 10:41:13 +0800

    add file passing_arguments.py

---
+++ passing_arguments.py
@@ -0,0 +1,3 @@
+def myfunction():
+	print('this is a word')
+myfunction()
--- server.py
+++ server.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+import mypalindrome
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/api/palindrome/")
+def palindrome():
+  return mypalindrome.help()
+
+@app.route('/')
+def index():
+	return render_template('index.html')
