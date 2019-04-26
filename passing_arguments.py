0235a669f4ed9fe4165ee941d6f09e85e3ed523a
major <echolabstech@gmail.com>
2019-04-26 10:49:29 +0800

    show passing array arugment

--- passing_arguments.py
+++ passing_arguments.py
@@ -1,3 +1,7 @@
 def myfunction(word, number):
 	print('this is a {}, and this is a number: {}'.format(word, number))
 myfunction('word', 122)
+
+def myfunction2(args):
+	print(args)
+myfunction2(['dad'])
