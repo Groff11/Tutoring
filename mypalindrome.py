5facff0c64653473c2d4b4bba4b6325a144b6202
major <echolabstech@gmail.com>
2019-05-15 10:20:06 +0800

    comments explaining boolean expression, remove uneccessary parenthesis, and rename uppercase variable to lowercase

--- mypalindrome.py
+++ mypalindrome.py
@@ -1,20 +1,20 @@
 # return True if palindrome, False if not
-def help(word):
+def help(word=''):
 	first_word = word
-	while (first_word) != (bad):
-		Palindrome = (1)
+	while first_word != bad: # boolean expression - expression of True or False
+		palindrome = 1
 		while len(first_word) > 1:
 			first = first_word[0]
 			last = first_word[len(first_word)-1]
 			if first != last:
-				Palindrome = (0)
+				palindrome = 0
 				break
 			else:
-				Palindrome = (1)
+				palindrome = 1
 				del first_word (0)
 				first_word.pop()
-		first_word = (bad)
-		if Palindrome == (1):
+		first_word = bad
+		if palindrome == 1:
 			first_word = "".join(str(x) for x in first_word)
 			print ("{0} is a palindrome".format(first_word))
 			return content, status.HTTP_200_OK
