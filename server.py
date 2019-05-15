b1180f2912347267f9027676b6245bdb507e061a
major <echolabstech@gmail.com>
2019-05-15 10:51:41 +0800

    endpoint returns status code if palindrome

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, status
 app = Flask(__name__)
 import mypalindrome
 
 @app.route("/api/palindrome/")
 def palindrome():
-  return mypalindrome.help()
+  is_palindrome = mypalindrome.help()
+  if is_palindrome:
+		return status.HTTP_200_OK
+	else:
+		return status.HTTP_406_NOT_ACCEPTABLE
 
 @app.route('/')
 def index():
